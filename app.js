c1297cdd285f629f3d4f10227b38a75bac419de2
Akhil Konduru <60151778+maverick1910@users.noreply.github.com>
2020-12-13 23:34:35 +0530

    Back-end code added

---
+++ app.js
@@ -0,0 +1,19 @@
+//Packages Config 
+var     PORT= process.env.PORT || 3000
+        express =require('express'),
+        bodyparser=require("body-parser"),
+        app=express();
+
+//App Config 
+app.set("view engine","ejs");
+app.use('*/public', express.static('public/'));
+app.use(bodyparser.urlencoded({extended:true}));
+
+//Database Schema 
+
+//Routes Config 
+app.get('/',function(req,res){
+    res.render('homepage');
+});
+
+app.listen(PORT,console.log('Server Up!!'));
